80ce254dfe426a5715432521b6c55fc13b9485a7
CubeSuite <cubesuite@outlook.com>
2023-07-30 12:58:43 +0100

    Added popup windows for showing messages and getting confirmation

--- Techtonica Mod Loader/Utils.cs
+++ Techtonica Mod Loader/Utils.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Techtonica_Mod_Loader.Windows;
 
 namespace Techtonica_Mod_Loader
 {
@@ -18,6 +19,33 @@ namespace Techtonica_Mod_Loader
         public static void SendDebugLine(string Str) {
             Console.WriteLine(Str);
             Debug.WriteLine(Str);
+        }
+    }
+
+    public static class GuiUtils 
+    {
+        public static void showShader() {
+            MainWindow.current.shader.Visibility = System.Windows.Visibility.Visible;
+        }
+
+        public static void hideShader() {
+            MainWindow.current.shader.Visibility = System.Windows.Visibility.Hidden;
+        }
+
+        public static void showInfoMessage(string title, string description, string closeButtonText = "Close") {
+            WarningWindow.showInfo(title, description, closeButtonText);
+        }
+
+        public static void showWarningMessage(string title, string description, string closeButtonText = "Close") {
+            WarningWindow.showWarning(title, description, closeButtonText);
+        }
+
+        public static void showErrorMessage(string title, string description, string closeButtonText = "Close") {
+            WarningWindow.showError(title, description, closeButtonText);
+        }
+
+        public static bool getUserConfirmation(string title, string description) {
+            return GetYesNoWindow.getYesNo(title, description);
         }
     }
 }
